2e45daff289393ad8f3082247a33c531aa0069cf
itegrootenhuis <itegrootenhuis@bizstream.com>
2018-03-05 11:46:23 -0500

    Project init

---
+++ WhatchaWatchin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhatchaWatchin.Startup))]
+namespace WhatchaWatchin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

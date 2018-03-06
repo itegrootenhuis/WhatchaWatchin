b4a2ecb8aa69d1e06c13833248efbd729b5eaef5
Adam Tasma <adamtasma@gmail.com>
2018-03-06 17:08:05 -0500

    added api object creation and the baseMovieSurvey.json file

--- WhatchaWatchin/Controllers/HomeController.cs
+++ WhatchaWatchin/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +29,35 @@ namespace WhatchaWatchin.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult SearchMovie(string title)
+        {
+            //HttpWebRequest request = WebRequest.CreateHttp("http://www.omdbapi.com/?apikey=d0069624&t=titanic");
+            HttpWebRequest request = WebRequest.CreateHttp("http://www.omdbapi.com/?apikey=d0069624&t=" + title);
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            String data = rd.ReadToEnd();
+            JObject o = JObject.Parse(data);
+
+            JToken _title = o["Title"];
+            JToken _plot = o["Plot"];
+            JToken _poster = o["Poster"];
+            JToken _genre = o["Genre"];
+            JToken _type = o["Type"];
+            JToken _year = o["Year"];
+            JToken _mpaaRating = o["Rated"];
+            JToken _runtime = o["Runtime"];
+            JToken _language = o["Language"];
+            JToken _imdbRating = o["imdbRating"];
+            JToken _website = o["Website"];
+            JToken _imdbID = o["imdbID"];
+
+            Movie m = new Movie(_title.ToString(), _plot.ToString(), _poster.ToString(), _genre.ToString(), _type.ToString(), int.Parse(_year.ToString()), _mpaaRating.ToString(), _runtime.ToString(), _language.ToString(), _imdbRating.ToString(), _website.ToString(), _imdbID.ToString());
+
+            return View("QuizResult");
         }
     }
 }
